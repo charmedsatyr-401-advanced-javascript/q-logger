e91ff3fb7bc3d711f959bfad9538a5194bdcec81
Joseph <z@charmed.tech>
2019-04-13 09:13:30 -0700

    Install dotenv

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
+// Environment
+require('dotenv').config();
+
+// Import client-side Q
 const Q = require('@nmq/q/client');
 
+// Subscribe to events in the `database` namespace
 const db = new Q('database');
-const files = new Q('files');
-
-// Subscribe to `db` events
 db.subscribe('create', payload => console.log('create in `db`:', payload));
 
 db.subscribe('read', payload => console.log('read in `db`:', payload));
@@ -16,7 +18,8 @@ db.subscribe('delete', payload => console.log('delete in `db`:', payload));
 
 db.subscribe('error', payload => console.log('error in `db`:', payload));
 
-// Subscribe to `files` events
+// Subscribe to events in the `files` namespace
+const files = new Q('files');
 files.subscribe('save', payload => console.log('save in `files`:', payload));
 files.subscribe('error', payload => console.log('error in `files`:', payload));
 
