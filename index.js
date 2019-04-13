0a5a36015489638a65b5d1b7269fa8420e0fa449
CharmedSatyr <16056963+CharmedSatyr@users.noreply.github.com>
2019-04-13 10:07:38 -0700

    Standardize newlines, improve console logging

--- index.js
+++ index.js
@@ -8,21 +8,17 @@ const Q = require('@nmq/q/client');
 
 // Subscribe to events in the `database` namespace
 const db = new Q('database');
-db.subscribe('create', payload => console.log('create in `db`:', payload));
-
-db.subscribe('read', payload => console.log('read in `db`:', payload));
-
-db.subscribe('update', payload => console.log('update in `db`:', payload));
-
-db.subscribe('delete', payload => console.log('delete in `db`:', payload));
-
-db.subscribe('error', payload => console.log('error in `db`:', payload));
+db.subscribe('create', payload => console.log('`create` event in `db`:', payload));
+db.subscribe('read', payload => console.log('`read` event in `db`:', payload));
+db.subscribe('update', payload => console.log('`update` event in `db`:', payload));
+db.subscribe('delete', payload => console.log('`delete` event in `db`:', payload));
+db.subscribe('error', payload => console.log('`error` event in `db`:', payload));
 
 // Subscribe to events in the `files` namespace
 const files = new Q('files');
-files.subscribe('save', payload => console.log('save in `files`:', payload));
-files.subscribe('error', payload => console.log('error in `files`:', payload));
+files.subscribe('save', payload => console.log('`save` event in `files`:', payload));
+files.subscribe('error', payload => console.log('`error` event in `files`:', payload));
 
 // See all subscriptions
-console.log(db.subscriptions());
-console.log(files.subscriptions());
+console.log('database subscriptions:', db.subscriptions());
+console.log('files subscriptions:', files.subscriptions());
